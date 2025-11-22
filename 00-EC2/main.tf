fce998db6756fead37185e59209eaa0a9fd6c266
Manikanta Akula <maniakula202@gmail.com>
2025-11-22 20:53:39 +0530

    Terraform

--- 00-EC2/main.tf
+++ 00-EC2/main.tf
@@ -21,19 +21,11 @@ resource "aws_security_group" "docker_sg_group" {
   description = "SSH traffic"
 
   ingress {
-    description = "Allow SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "Allow SSH"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    description       = "Allow all inbound traffic from anywhere"
+    cidr_blocks       = ["0.0.0.0/0"]
+    from_port         = 0
+    to_port           = 0
+    protocol          = "-1" # Represents all protocols
   }
 
   egress {
